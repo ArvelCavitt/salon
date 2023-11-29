1b7b68cc304631b819729c76f295645555654457
Arvel Cavitt <arvelcavitt4@gmail.com>
2023-11-28 20:51:24 -0800

    commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import render_template, request, redirect, session, flash
-from flask_app.models import User
+from flask_app.models import user
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
