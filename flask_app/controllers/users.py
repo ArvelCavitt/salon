b51141a76ed9bf678e3574b067514def7b721b44
Arvel Cavitt <arvelcavitt4@gmail.com>
2023-11-28 23:28:10 -0800

    commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -7,3 +7,19 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def log():
     return render_template('log.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = user.User.get_email(request.form)
+    if not user:
+        flash("Invalid email", "login")
+        return redirect('/')
+    if bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid password", "login")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect('/dashboard')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
