06609ece780eb1b4a4d47465d2f7098babf53b29
Arvel Cavitt <arvelcavitt4@gmail.com>
2023-12-22 22:37:37 -0800

    commit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import render_template, request, redirect, session, flash
 from flask_app.models import user
+from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
@@ -20,7 +21,7 @@ def logout():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    user = user.User.get_email(request.form)
+    user = User.get_email(request.form)
     if not user:
         flash("Invalid email", "login")
         return redirect('/')
