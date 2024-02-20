3dad9d36c01f352ef656495a7fc55278f2c895c7
Arvel Cavitt <arvelcavitt4@gmail.com>
2024-02-19 21:55:44 -0800

    commit

--- flask_app/controllers/services.py
+++ flask_app/controllers/services.py
@@ -5,3 +5,24 @@ from flask_app.models import user, service
 @app.route('/book_with_me')
 def book():
     return render_template("booking.html")
+
+@app.route("/book_with_me", methods=['POST'])
+def create_booking():
+    booking_appointment = request.form
+    if not service.Service.is_valid(booking_appointment):
+        return redirect("/book_with_me")
+    if "user_id" not in session:
+        return redirect("/book_with_me")
+    data = {
+        "cut": request.form["cut"],
+        "color": request.form["color"],
+        "description": request.form["description"],
+        "date": request.form["date"],
+        "user_id": request.form["user_id"]
+    }
+    service.Service.create_new_service(data)
+    return redirect("/confirmation")
+
+@app.route("/confirmation")
+def confirm():
+    return render_template("confirmation.html")
